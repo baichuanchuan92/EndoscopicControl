d6ac0e8cb343225363a885230f24fb1f1affb5e2
baichuanchuan92 <baichuan_math@outlook.com>
2020-12-14 14:53:36 +0800

    修复、新添
    1.长短直线电机允许互斥操作，并且正常运行。
    2.添加CRC校验。
    3.对写入消息的部分，数据区域的移位运算有问题。

--- EndoscopicControl/Form1.Designer.cs
+++ EndoscopicControl/Form1.Designer.cs
@@ -43,23 +43,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1167, 298);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(584, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 84);
+            this.button1.Size = new System.Drawing.Size(66, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "一起动";
+            this.button1.Text = "复位";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(45, 174);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(6);
+            this.trackBar1.Location = new System.Drawing.Point(22, 87);
             this.trackBar1.Maximum = 4002;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(824, 90);
-            this.trackBar1.SmallChange = 100;
+            this.trackBar1.Size = new System.Drawing.Size(412, 45);
+            this.trackBar1.SmallChange = 200;
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Value = 2001;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -68,18 +67,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(41, 85);
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 41);
+            this.label1.Size = new System.Drawing.Size(42, 21);
             this.label1.TabIndex = 3;
             this.label1.Text = "弯曲";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1167, 476);
+            this.button2.Location = new System.Drawing.Point(584, 238);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 87);
+            this.button2.Size = new System.Drawing.Size(66, 44);
             this.button2.TabIndex = 4;
             this.button2.Text = "测试";
             this.button2.UseVisualStyleBackColor = true;
@@ -89,50 +90,51 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(44, 494);
+            this.label2.Location = new System.Drawing.Point(22, 247);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 41);
+            this.label2.Size = new System.Drawing.Size(42, 21);
             this.label2.TabIndex = 6;
             this.label2.Text = "旋转";
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(48, 583);
-            this.trackBar2.Margin = new System.Windows.Forms.Padding(6);
+            this.trackBar2.Location = new System.Drawing.Point(24, 292);
             this.trackBar2.Maximum = 4002;
             this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(824, 90);
+            this.trackBar2.Size = new System.Drawing.Size(412, 45);
             this.trackBar2.SmallChange = 100;
             this.trackBar2.TabIndex = 5;
             this.trackBar2.Value = 2001;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(1039, 29);
+            this.label3.Location = new System.Drawing.Point(520, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 41);
+            this.label3.Size = new System.Drawing.Size(42, 21);
             this.label3.TabIndex = 8;
             this.label3.Text = "前后";
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(961, 15);
-            this.trackBar3.Margin = new System.Windows.Forms.Padding(6);
+            this.trackBar3.Location = new System.Drawing.Point(480, 8);
             this.trackBar3.Maximum = 4002;
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar3.Size = new System.Drawing.Size(90, 824);
+            this.trackBar3.Size = new System.Drawing.Size(45, 412);
             this.trackBar3.SmallChange = 100;
             this.trackBar3.TabIndex = 7;
             this.trackBar3.Value = 2001;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1327, 874);
+            this.ClientSize = new System.Drawing.Size(664, 437);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.trackBar3);
             this.Controls.Add(this.label2);
@@ -141,7 +143,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "内镜控制";
             this.Load += new System.EventHandler(this.Form1_Load);
--- EndoscopicControl/Form1.cs
+++ EndoscopicControl/Form1.cs
@@ -36,8 +36,12 @@ namespace EndoscopicControl
             //bm.AddStickAndPara(2, 500);
             //bm.Process();
             //MessageSend.SendMessage((MessageGenric)bm);
-            Stick stick = MessageReceive.FindStickByID(1);
-            stick.SetStickLegth((uint)500);
+            Stick stick;
+            stick = MessageReceive.FindStickByID(m_leftBend);
+            stick.SetStickLegth((uint)2000);
+            stick = MessageReceive.FindStickByID(m_rightBend);
+            stick.SetStickLegth((uint)2000);
+
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
@@ -75,8 +79,19 @@ namespace EndoscopicControl
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Stick stick = MessageReceive.FindStickByID(3);
-            stick.resetStickID(1);
+            //Stick stick = MessageReceive.FindStickByID(1);
+            //stick.resetStickID(2);
+            Stick stick = MessageReceive.FindStickByID(2);
+            stick.SetStickLegth((uint)1700);
+            Stick stick1 = MessageReceive.FindStickByID(1);
+            stick1.SetStickLegth((uint)1700);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            //QueryMessage mm = new QueryMessage(1, VSMDMessage.CMD_TYPE.CMD_RD_STATE, VSMDMessage.STATE_TYPE.STATE_CUR_VEL,20);
+            WriteMessage aa = new WriteMessage(1, VSMDMessage.CMD_TYPE.CMD_WR_MULTIES, VSMDMessage.STATE_TYPE.STATE_CUR_LOC, 2, 4);
+
         }
     }
 }
